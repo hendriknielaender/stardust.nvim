e695f34d6972330a0e601e4f98706965645fbfae
Hendrik Nieländer <hendrik@nielaender.de>
2024-06-25 00:11:36 +0200

    fix: colors

--- lua/stardust/colors.lua
+++ lua/stardust/colors.lua
@@ -1,41 +1,37 @@
 return {
-  fg = "#dcdccc", -- Using the 'Normal' foreground color
-  bg = "#18181b", -- Using the 'Normal' background color
-  dark = "#121212", -- An approximation from CursorLine background for dark areas
-  accent = "#ffa263", -- Common accent color used in the theme (e.g., Boolean, Typedef, Type)
-  popup_back = "#2e3330",
-  cursor_fg = "#000d18", -- From 'Cursor' foreground
-  cursor_bg = "#8faf9f", -- From 'Cursor' background
-  error_fg = "#e37170", -- From 'Error' foreground
-  error_bg = "#3d3535", -- From 'Error' background
-  warning_fg = "#ffffff", -- From 'WarningMsg' foreground
-  warning_bg = "#333333", -- From 'WarningMsg' background
-  info_fg = "#ffffff", -- Approximation for informational texts
-  info_bg = "#2f2f2f", -- Approximation from 'ErrorMsg' background
-  hint_fg = "#ccccbc",
-  hint_bg = "#242424",
-  success_fg = "#ccdc90",
-  success_bg = "#353a37",
-  visual_fg = "#ffffe0", -- From 'Search' foreground
-  visual_bg = "#284f28", -- From 'Search' background
-  replace_fg = "#FFE636", -- From 'IncSearch' foreground
-  replace_bg = "#385f38", -- From 'IncSearch' background
-  terminal_colors = {
-    "#1f1f1f",
-    "#ac4142",
-    "#90a959",
-    "#f4bf75",
-    "#6a9fb5",
-    "#8f8f8f",
-    "#75b5aa",
-    "#dcdccc",
-    "#6f6f6f",
-    "#ecb3b3",
-    "#f4bf75",
-    "#6a9fb5",
-    "#8f8f8f",
-    "#75b5aa",
-    "#dcdccc",
-    "#ffcfaf",
-  },
+  fg = "#D4D4D4",
+  bg = "#1E1E1E",
+  dark = "#252525",
+  accent = "#BBBBBB",
+  popup_back = "#2D2D30",
+  search_orange = "#613214",
+  search_blue = "#5e81ac",
+  gray = "#808080",
+  dark_gray = "#3e3e3e",
+  context = "#606060",
+  light_gray = "#c8c9c1",
+  blue = "#569CD6",
+  dark_blue = "#223E55",
+  light_blue = "#9CDCFE",
+  cyan = "#4EC9B0",
+  green = "#6A9955",
+  light_green = "#B5CEA8",
+  red = "#F44747",
+  light_red = "#D16969",
+  orange = "#CE9178",
+  yellow = "#DCDCAA",
+  yellow_orange = "#D7BA7D",
+  purple = "#C586C0",
+  magenta = "#D16D9E",
+  cursor_fg = "#515052",
+  cursor_bg = "#AEAFAD",
+  error_red = "#F44747",
+  warning_orange = "#ff8800",
+  info_yellow = "#FFCC66",
+  hint_blue = "#4FC1FF",
+  ui_blue = "#264F78",
+  diff_add = "#587c0c",
+  diff_change = "#0c7d9d",
+  diff_delete = "#94151b",
+  diff_text = "#265478",
 }
